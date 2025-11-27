b8924a504ba0189d4204c391da64d6f3d5b8d8f9
mungrewal <munmeetgrewal21@gmail.com>
2025-11-27 16:05:00 -0500

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,1 +1,15 @@
+#COMP-3110, Final Project, Step 1 : Preprocessing implementation
+#preprocess.py
+#Author : Munmeet Grewal
 
+
+import rec
+from typing import Iterable, List
+
+WHITESPACE_RUN = re.compile(r"\st") 
+
+def remove_whitespace(text: str)-> str :
+  return WHITESPACE_RUN.sub(" ", text)
+
+def cleanUp_line(raw_line: str) -> str:
+  line = 
